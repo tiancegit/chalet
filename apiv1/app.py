dc90a3eb76b3bc7bb59d5daec058dd7133627b88
x1ahgxq <3223340031@qq.com>
2017-11-16 21:26:51 +0800

    项目骨架初始化

---
+++ apiv1/app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from flask import Flask
+
+# from apiv1.views import blueprint_first
+
+
+app = Flask(__name__)
+app.config.from_object("apiv1.config")
+
+# register blueprint
+# app.register_blueprint(blueprint_first, prefix='/first')
+
+
+
+@app.before_request
+def before_request():
+    pass
+
+
+@app.after_request
+def after_request(resp):
+    return resp
+
+
+@app.errorhandler(404)
+def page_not_found(err):
+    return '404'
---
+++ apiv1/run_server.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+
+import sys
+
+from apiv1.app import app
+
+
+def main():
+    try:
+        port = int(sys.argv[1])
+    except:
+        port = None
+    app.run(host='0.0.0.0', port=port or 8000)
+
+
+if __name__ == "__main__":
+    main()
